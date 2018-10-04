46f97c5bb5fff1e813bf37ac76527c10eb0bbafb
Kumar Shaswat <42195352+KrShaswat@users.noreply.github.com>
2018-10-04 15:23:17 +0530

    Add files via upload

---
+++ PSET2 P1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  4 09:44:50 2018
+Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+
+The following variables contain values as described below:
+
+balance - the outstanding balance on the credit card
+
+annualInterestRate - annual interest rate as a decimal
+
+monthlyPaymentRate - minimum monthly payment rate as a decimal
+
+For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining balance. Be sure to print out no more than two decimal digits of accuracy - so print
+
+Monthly interest rate= (Annual interest rate) / 12.0
+Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+@author: kumar
+"""
+balance = 484
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+
+for n in range (12):
+    balance = (balance-(monthlyPaymentRate*balance))+((annualInterestRate/12)*(balance-(monthlyPaymentRate*balance)))
+print("Remaining Balance: ", round(balance, 2))
